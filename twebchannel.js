f40b31ac5e98cfeff32cbb3d3a8bda303a30cea4
Rogério Ribeiro da Cruz <rogeriorc@gmail.com>
2019-08-13 11:45:17 -0300

    Ajuste na versao

--- twebchannel.js
+++ twebchannel.js
@@ -8,7 +8,7 @@ function QObject(e, n, t) { function o(e, n) { var o = e[0], r = e[1]; i[o] = { 
 
 var twebchannel = {
 	// Versao
-	version: "1.0.0",
+	version: "1.0.1",
 
 	// Porta do WebSocket
 	internalPort: 0,
